ebb0415a799f0e0f300ba627491cc8a497ee9eaf
Katie Mangan <katie.mangan1@gmail.com>
2019-03-09 11:14:19 -0600

    conditionally render database columns on DOM

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -4,25 +4,53 @@ import { connect } from 'react-redux';
 
 class ProjectItem extends Component {
 
-
-
-
-
-
-
-
-
-
     render() {
+        let descriptionDisplay;
+        let thumbnailDisplay;
+        let websiteDisplay;
+        let githubDisplay;
+
+        if (this.props.project.description === '') {
+            descriptionDisplay = null;
+        }
+        else {
+            descriptionDisplay = this.props.project.description;
+        }
+
+        if (this.props.project.thumbnail === '') {
+            thumbnailDisplay = null;
+        }
+        else {
+            thumbnailDisplay = this.props.project.thumbnail;
+        }
+
+        if (this.props.project.website === '') {
+            websiteDisplay = null;
+        }
+        else {
+            websiteDisplay = this.props.project.website;
+        }
+
+        if (this.props.project.github === '') {
+            githubDisplay = null;
+        }
+        else {
+            githubDisplay = this.props.project.github;
+        }
+
         return (
             <div>
-                <div>{this.props.project.name}</div>
+                {descriptionDisplay}
+                <br />
+                {thumbnailDisplay}
+                <br />
+                {websiteDisplay}
+                <br/>
+                {githubDisplay}
             </div>
         );
     }
 }
-
-
 
 
 
